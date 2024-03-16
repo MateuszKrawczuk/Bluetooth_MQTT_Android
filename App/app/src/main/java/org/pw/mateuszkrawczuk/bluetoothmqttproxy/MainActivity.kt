d10bf3d816439b7ab5fd7cdfeda5a89c4284753b
Mateusz Krawczuk <mat.krawczuk@gmail.com>
2024-03-16 21:54:59 +0100

    Add: Scanning and clearing list of found devices
    Signed-off-by: Mateusz Krawczuk <mat.krawczuk@gmail.com>

--- App/app/src/main/java/org/pw/mateuszkrawczuk/bluetoothmqttproxy/MainActivity.kt
+++ App/app/src/main/java/org/pw/mateuszkrawczuk/bluetoothmqttproxy/MainActivity.kt
@@ -6,18 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import dagger.hilt.android.AndroidEntryPoint
+import org.pw.mateuszkrawczuk.bluetoothmqttproxy.model.BluetoothDevice
 import org.pw.mateuszkrawczuk.bluetoothmqttproxy.ui.theme.BluetoothMQTTProxyTheme
 import org.pw.mateuszkrawczuk.bluetoothmqttproxy.viewmodel.MainActivityViewModel
 import javax.inject.Inject
@@ -26,14 +31,17 @@ import javax.inject.Inject
 class MainActivity : ComponentActivity() {
 
     @Inject
-    lateinit  var viewModel: MainActivityViewModel
+    lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             BluetoothMQTTProxyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Sample(viewModel)
                 }
             }
@@ -42,54 +50,104 @@ class MainActivity : ComponentActivity() {
 }
 
 
-    @OptIn(ExperimentalPermissionsApi::class)
-    @Composable
-    private fun Sample(viewModel: MainActivityViewModel) {
-        val locationPermissionsState = rememberMultiplePermissionsState(
-            listOf(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.BLUETOOTH_SCAN,
-                android.Manifest.permission.BLUETOOTH_CONNECT,
-            )
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun Sample(viewModel: MainActivityViewModel) {
+    val locationPermissionsState = rememberMultiplePermissionsState(
+        listOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.BLUETOOTH_SCAN,
+            android.Manifest.permission.BLUETOOTH_CONNECT,
         )
+    )
 
-        if (locationPermissionsState.allPermissionsGranted) {
-            Text("Thanks! I can access your exact location :D")
-            viewModel.scan()
+    if (locationPermissionsState.allPermissionsGranted) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text("Thanks! I can access your exact location.")
+            Text("Devices found: ${viewModel.deviceList.size}")
+            Button(onClick = { viewModel.scan() }) {
+                Text("Scan")
 
-        } else {
-            Column {
-                val allPermissionsRevoked =
-                    locationPermissionsState.permissions.size ==
-                            locationPermissionsState.revokedPermissions.size
+            }
+            Button(onClick = { viewModel.cleanDeviceList() }) {
+                Text("Clean")
+            }
+            if (viewModel.deviceList.isEmpty()) {
+                Text("No devices found")
+            } else {
+                DeviceList(viewModel)
+            }
 
-                val textToShow = if (!allPermissionsRevoked) {
-                    // If not all the permissions are revoked, it's because the user accepted the COARSE
-                    // location permission, but not the FINE one.
-                    "Yay! Thanks for letting me access your approximate location. " +
-                            "But you know what would be great? If you allow me to know where you " +
-                            "exactly are. Thank you!"
-                } else if (locationPermissionsState.shouldShowRationale) {
-                    // Both location permissions have been denied
-                    "Getting your exact location is important for this app. " +
-                            "Please grant us fine location. Thank you :D"
-                } else {
-                    // First time the user sees this feature or the user doesn't want to be asked again
-                    "This feature requires location permission"
-                }
+            Text("Scanning for: ${viewModel.scanCounter.value} seconds")
 
-                val buttonText = if (!allPermissionsRevoked) {
-                    "Allow precise location"
-                } else {
-                    "Request permissions"
-                }
+        }
 
-                Text(text = textToShow)
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(onClick = { locationPermissionsState.launchMultiplePermissionRequest() }) {
-                    Text(buttonText)
-                }
+    } else {
+        Column {
+            val allPermissionsRevoked =
+                locationPermissionsState.permissions.size ==
+                        locationPermissionsState.revokedPermissions.size
+
+            val textToShow = if (!allPermissionsRevoked) {
+                // If not all the permissions are revoked, it's because the user accepted the COARSE
+                // location permission, but not the FINE one.
+                "Yay! Thanks for letting me access your approximate location. " +
+                        "But you know what would be great? If you allow me to know where you " +
+                        "exactly are. Thank you!"
+            } else if (locationPermissionsState.shouldShowRationale) {
+                // Both location permissions have been denied
+                "Getting your exact location is important for this app. " +
+                        "Please grant us fine location. Thank you :D"
+            } else {
+                // First time the user sees this feature or the user doesn't want to be asked again
+                "This feature requires location permission"
+            }
+
+            val buttonText = if (!allPermissionsRevoked) {
+                "Allow precise location"
+            } else {
+                "Request permissions"
+            }
+
+            Text(text = textToShow)
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = { locationPermissionsState.launchMultiplePermissionRequest() }) {
+                Text(buttonText)
             }
         }
     }
+}
+
+@Composable
+private fun DeviceList(viewModel: MainActivityViewModel) {
+    Column {
+        viewModel.deviceList.forEach {
+            BleDevice(it)
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
+
+@Composable
+private fun BleDevice(device: BluetoothDevice) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text("Name: ${device.name}")
+            Text("Address: ${device.address}")
+        }
+    }
+
+}
--- App/app/src/main/java/org/pw/mateuszkrawczuk/bluetoothmqttproxy/viewmodel/MainActivityViewModel.kt
+++ App/app/src/main/java/org/pw/mateuszkrawczuk/bluetoothmqttproxy/viewmodel/MainActivityViewModel.kt
@@ -1,29 +1,83 @@
 package org.pw.mateuszkrawczuk.bluetoothmqttproxy.viewmodel
 
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.polidea.rxandroidble3.RxBleClient
+import com.polidea.rxandroidble3.scan.ScanFilter
+import com.polidea.rxandroidble3.scan.ScanSettings
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.pw.mateuszkrawczuk.bluetoothmqttproxy.model.BluetoothDevice
 import timber.log.Timber
 import javax.inject.Inject
 
 
 class MainActivityViewModel @Inject constructor (val rxBleClient: RxBleClient ) : ViewModel() {
 
+    val deviceList = mutableStateListOf<BluetoothDevice>()
+    val scanCounter = mutableStateOf(0)
 
-    fun scan(){
+    fun scan() {
+        viewModelScope.launch {
+            _scan()
+        }
+    }
 
-        val x =  rxBleClient.scanBleDevices()
+    fun removeDevice(device: BluetoothDevice) {
+        deviceList.remove(device)
+    }
 
+    fun cleanDeviceList() {
+        deviceList.clear()
+    }
+
+    /**
+     * Scans for BLE devices
+     *
+     */
+    suspend fun _scan() {
+        Timber.i("Prepare to scan...")
+        val scanSettings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+        val scanFilter: ScanFilter = ScanFilter.Builder().build()
+        val x = rxBleClient.scanBleDevices(scanSettings, scanFilter)
         Timber.i( "scan: $x")
-        x.subscribe(
+        val result = x.subscribe(
             { scanResult ->
                 // Process scan result here.
                 Timber.i("scan: $scanResult")
+                Log.i("", "scan: $scanResult")
+                val newDevice = BluetoothDevice(
+                    scanResult.bleDevice.name ?: "Unknown",
+                    scanResult.bleDevice.macAddress
+                )
+                val existingDevice = deviceList.find { it.address == newDevice.address }
 
+                if (existingDevice == null) {
+                    // If the device does not exist in the list, add it
+                    deviceList.add(newDevice)
+                } else if (existingDevice.name == "Unknown" && newDevice.name != "Unknown") {
+                    // If the device exists and its name is "Unknown", update the name
+                    val updatedDevice = existingDevice.copy(name = newDevice.name)
+                    deviceList[deviceList.indexOf(existingDevice)] = updatedDevice
+                }
             },
             { throwable ->
                 // Handle an error here.
                 Timber.i( "scan: $throwable")
             }
         )
+        scanCounter.value = 5
+        while (scanCounter.value > 0) {
+            delay(1000)
+            scanCounter.value -= 1
+        }
+        result.dispose()
+
     }
 }
