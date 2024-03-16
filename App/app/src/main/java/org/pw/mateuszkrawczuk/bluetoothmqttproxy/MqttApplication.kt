47334f1d3556fb9861875538cfa43ebc93b7b060
Mateusz Krawczuk <mat.krawczuk@gmail.com>
2024-03-16 20:33:57 +0100

    Add: Timber for logs
    Signed-off-by: Mateusz Krawczuk <mat.krawczuk@gmail.com>

--- App/app/src/main/java/org/pw/mateuszkrawczuk/bluetoothmqttproxy/MqttApplication.kt
+++ App/app/src/main/java/org/pw/mateuszkrawczuk/bluetoothmqttproxy/MqttApplication.kt
@@ -1,7 +1,18 @@
 package org.pw.mateuszkrawczuk.bluetoothmqttproxy
 
 import android.app.Application
+import android.util.Log
+import com.polidea.rxandroidble3.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
 
 @HiltAndroidApp
-class MqttApplication : Application() {}
+class MqttApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
--- App/app/src/main/java/org/pw/mateuszkrawczuk/bluetoothmqttproxy/viewmodel/MainActivityViewModel.kt
+++ App/app/src/main/java/org/pw/mateuszkrawczuk/bluetoothmqttproxy/viewmodel/MainActivityViewModel.kt
@@ -1,26 +1,28 @@
 package org.pw.mateuszkrawczuk.bluetoothmqttproxy.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.polidea.rxandroidble3.RxBleClient
+import timber.log.Timber
 import javax.inject.Inject
 
 
 class MainActivityViewModel @Inject constructor (val rxBleClient: RxBleClient ) : ViewModel() {
 
+
     fun scan(){
 
         val x =  rxBleClient.scanBleDevices()
 
-        Log.i("MainActivityViewModel", "scan: $x")
+        Timber.i( "scan: $x")
         x.subscribe(
             { scanResult ->
                 // Process scan result here.
-                Log.i("MainActivityViewModel", "scan: $scanResult")
+                Timber.i("scan: $scanResult")
+
             },
             { throwable ->
                 // Handle an error here.
-                Log.i("MainActivityViewModel", "scan: $throwable")
+                Timber.i( "scan: $throwable")
             }
         )
     }
